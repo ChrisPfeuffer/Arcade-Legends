800ff534a52b0d882e4ae54a29e2c1618d534189
ChrisPfeuffer <chris_pfeuffer@web.de>
2021-01-06 18:29:02 +0100

    walljump but not finished

--- Arcade Legends/Assets/Scripts/PlayerController.cs
+++ Arcade Legends/Assets/Scripts/PlayerController.cs
@@ -12,7 +12,6 @@ public class PlayerController : MonoBehaviour
     private float jumpHeight = 0.7f;
     private float gravityValue = -15f;
     private float rotationSpeed = 10f;
-
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
@@ -42,7 +41,7 @@ public class PlayerController : MonoBehaviour
             playerVelocity.y = 0f;
         }
         Vector2 movement = movementControl.action.ReadValue<Vector2>();
-        Vector3 move = new Vector3(movement.x, 0,movement.y);
+        Vector3 move = new Vector3(movement.x, 0, movement.y);
         move = cameraMainTransform.forward * move.z + cameraMainTransform.right * move.x;
         move.y = 0f;
         controller.Move(move * Time.deltaTime * playerSpeed);
@@ -57,11 +56,27 @@ public class PlayerController : MonoBehaviour
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
 
-        if(movement != Vector2.zero)
+        if (movement != Vector2.zero)
         {
             float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cameraMainTransform.eulerAngles.y;
             Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
             transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
         }
+    }
+
+    #region Walljump
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+
+        if (!controller.isGrounded && hit.normal.y < 0.1f)
+        {
+
+            if (jumpControl.action.triggered)
+            {
+                Debug.DrawRay(hit.point, hit.normal, Color.red, 1.25f);
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            }
+        }
+        #endregion
     }
 }
