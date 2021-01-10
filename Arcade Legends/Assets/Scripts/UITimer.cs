1e8f32cd7670c03c04a99e8b46c99974d366141f
ChrisPfeuffer <chris_pfeuffer@web.de>
2021-01-10 19:42:34 +0100

    Timer script included

---
+++ Arcade Legends/Assets/Scripts/UITimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UITimer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    private float time;
+    void Start()
+    {
+        timerText = GetComponent<TextMeshProUGUI>();
+        timerText.text = "Time: " + time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
